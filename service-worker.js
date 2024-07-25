0e1a3d42818dbe1eb1f7911c685dc0e550bb07fb
BenyAlbatross <43571067+BenyAlbatross@users.noreply.github.com>
2024-07-25 10:35:43 +0800

    Add icon

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,49 @@
-const CACHE_NAME = 'risk-assessment-cache-v1';
-const urlsToCache = [
-    '/index.html',
-    '/styles.css',
-    '/script.js',
-    '/manifest.json',
+const cacheName = 'risk-assessment-cache-v1';
+const contentToCache = [
+    './',
+    './index.html',
+    './styles.css',
+    './script.js',
+    './manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.3.1/jspdf.umd.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.23/jspdf.plugin.autotable.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/signature_pad/5.0.2/signature_pad.umd.min.js'
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
+self.addEventListener("install", (e) => {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        (async () => {
+            const cache = await caches.open(cacheName);
+            console.log("[Service Worker] Caching all: app shell and content");
+            await cache.addAll(contentToCache);
+        })()
     );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        (async () => {
+            const r = await caches.match(e.request);
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+            if (r) { return r; }
+            const response = await fetch(e.request);
+            const cache = await caches.open(cacheName);
+            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+            cache.put(e.request, response.clone());
+            return response;
+        })()
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
     );
 });
